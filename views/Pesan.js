aac92a0e681ff65cceae40780371e60645a4f1b9
Arasya Rafi <94653668+GetSya@users.noreply.github.com>
2022-01-11 13:32:12 +0700

    Add files via upload

---
+++ views/Pesan.js
@@ -0,0 +1,16 @@
+try {
+	alert('Website Arasya');
+	alert('🗿');
+	alert('Hahahaa');
+	alert('Pencet aja trus🗿');
+	alert('Trus pencet');
+	alert('trus sampe ketemu website nya');
+	alert('DIKIT LAGI');
+	alert('3');
+	alert('2');
+	alert('1');
+	alert('Pencet oke');
+  
+}catch(e) {
+	console.log("pass")
+}
